c13c3438be1c1937d83171442c94ec834942ae74
shubyntsev-dev <shubyntsev1970@gmail.com>
2026-04-26 22:08:46 +0300

    Add some lessons

--- Learning_C_basics_App/Lessons_071-080.cs
+++ Learning_C_basics_App/Lessons_071-080.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Learning_C_basics_App
 {
@@ -35,7 +36,7 @@ namespace Learning_C_basics_App
             polyAnimal2.MakeSound(); // Выводит "Cat meow"
 
             Person2 person = new Person2();
-            
+
             person.Drive(new Car1());
 
             person.Drive(new SportCar());
@@ -93,7 +94,7 @@ namespace Learning_C_basics_App
             {
                 Console.WriteLine("Рон дон дон!");
             }
-            public override void  Drive()
+            public override void Drive()
             {
                 StartEngine();
                 Console.WriteLine("Я спортивная машина, я еду очень быстро!");
@@ -124,7 +125,7 @@ namespace Learning_C_basics_App
 
             foreach (var item in inventory)
             {
-                
+
                 player.CheckInfo(item);
                 player.Fire(item);
                 Console.WriteLine();
@@ -137,7 +138,7 @@ namespace Learning_C_basics_App
         {
             // Могут быть обычные свойства и методы с реализацией
             public abstract int Damage { get; }
-            
+
             // Абстрактный метод, который должен быть реализован в производных классах
             // Абстрактный метод может быть только в абстрактном классе и не имеет реализации в базовом классе.
             public abstract void Fire();
@@ -152,7 +153,7 @@ namespace Learning_C_basics_App
 
         class Gun1 : Weapon
         {
-            public override int Damage { get { return 5;  }  }
+            public override int Damage { get { return 5; } }
 
             public override void Fire()
             {
@@ -174,6 +175,7 @@ namespace Learning_C_basics_App
         {
             public override int Damage { get { return 3; } }
 
+            // HACK: example HACK
             public override void Fire()
             {
                 Console.WriteLine("Чпуньк!");
@@ -182,15 +184,334 @@ namespace Learning_C_basics_App
 
         class Player
         {
+            // UNDONE: example UNDONE
             public void Fire(Weapon weapon)
-            { 
+            {
                 weapon.Fire();
             }
-
+            // TODO: example TODO 
             public void CheckInfo(Weapon weapon)
             {
                 weapon.ShowInfo();
             }
+        }
+
+        // Интерфейсы и полиморфизм. Зачем нужны и как используются
+        public static void Lesson_073()
+        {
+            Console.WriteLine("Hello from Lesson_073  Интерфейсы и полиморфизм. Зачем нужны и как используются");
+            Console.WriteLine();
+
+            // Интерфейс - это контракт, который определяет набор методов и свойств, которые должны быть реализованы классами,
+            // которые его реализуют.
+            // Интерфейсы позволяют создавать гибкие и расширяемые архитектуры.
+
+            IDataProcessor dataProcessor = new ConsoleDataProcessor();
+            dataProcessor.ProcessData(new DbDataProvider());
+            dataProcessor.ProcessData(new FileDataProvider());
+            dataProcessor.ProcessData(new APIDataProvider());
+
+            Console.WriteLine();
+
+            PlayerLesson073 player = new PlayerLesson073();
+
+            WeaponLesson073[] inventory = { new GunLesson073(), new LaserGunLesson073(), new BowLesson073() };
+
+            foreach (var item in inventory)
+            {
+                player.Fire(item);
+                player.CheckInfo(item);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine(new string('-', 120));
+        }
+
+        interface IDataProvider
+        {
+            string GetData();
+        }
+
+        class DbDataProvider : IDataProvider
+        {
+            public string GetData()
+            {
+                return "Данные из базы данных";
+            }
+        }
+
+        class FileDataProvider : IDataProvider
+        {
+            public string GetData()
+            {
+                return "Данные из файла";
+            }
+        }
+
+        class APIDataProvider : IDataProvider
+        {
+            public string GetData()
+            {
+                return "Данные из API";
+            }
+        }
+
+        interface IDataProcessor
+        {
+            void ProcessData(IDataProvider dataProvider);
+        }
+
+        class ConsoleDataProcessor : IDataProcessor
+        {
+            public void ProcessData(IDataProvider dataProvider)
+            {
+                Console.WriteLine(dataProvider.GetData());
+            }
+
+        }
+
+        interface IHasInfo
+        {
+            void ShowInfo();
+        }
+        interface IWeapon
+        {
+            int Damage { get; }
+            void Fire();
+        }
+
+        // Класс WeaponLesson073 реализует интерфейсы IHasInfo и IWeapon,
+        // что позволяет использовать его объекты в полиморфном контексте.
+        abstract class WeaponLesson073 : IHasInfo, IWeapon
+        {
+            public abstract int Damage { get; }
+
+            public abstract void Fire();
+
+            public void ShowInfo()
+            {
+                Console.WriteLine($"{GetType().Name} Урон: {Damage}");
+            }
+        }
+
+        class GunLesson073 : WeaponLesson073
+        {
+            public override int Damage { get { return 5; } }
+
+            public override void Fire()
+            {
+                Console.WriteLine("Пыщ!");
+            }
+        }
+
+        class LaserGunLesson073 : WeaponLesson073
+        {
+            public override int Damage => 8;
+
+            public override void Fire()
+            {
+                Console.WriteLine("Пиу!");
+            }
+        }
+
+        class BowLesson073 : WeaponLesson073
+        {
+            public override int Damage { get { return 3; } }
+
+
+            public override void Fire()
+            {
+                Console.WriteLine("Чпуньк!");
+            }
+        }
+
+        class PlayerLesson073
+        {
+
+            public void Fire(WeaponLesson073 weapon)
+            {
+                weapon.Fire();
+            }
+
+            public void CheckInfo(WeaponLesson073 weapon)
+            {
+                weapon.ShowInfo();
+            }
+        }
+
+        // Наследование интерфейсов. Множественное наследование интерфейсов
+        public static void Lesson_074()
+        {
+            Console.WriteLine("Hello from Lesson_074  Наследование интерфейсов. Множественное наследование интерфейсов");
+            Console.WriteLine();
+
+            // Интерфейсы могут наследовать другие интерфейсы, что позволяет создавать более сложные контракты.
+            // Класс может реализовывать несколько интерфейсов, что обеспечивает множественное наследование интерфейсов.
+
+            PlayerLesson074 player = new PlayerLesson074();
+
+            IWeaponLesson074[] inventory = { new GunLesson074(), new LaserGunLesson074(), new KnifeLesson074() };
+
+            foreach (var item in inventory)
+            {
+                player.Fire(item);
+                Console.WriteLine();
+            }
+
+            player.Throw(new KnifeLesson074());
+
+
+            Console.WriteLine(new string('-', 120));
+        }
+
+        interface IWeaponLesson074
+        {
+            void Fire();
+        }
+
+        interface IThrowingWeaponLesson074 : IWeaponLesson074
+        {
+            void Throw();
+        }
+
+        class GunLesson074 : IWeaponLesson074
+        {
+            public void Fire()
+            {
+                Console.WriteLine($"{GetType().Name}: Пыщ!");
+            }
+        }
+
+        class LaserGunLesson074 : IWeaponLesson074
+        {
+            public void Fire()
+            {
+                Console.WriteLine($"{GetType().Name}: Пиу!");
+            }
+        }
+
+        class KnifeLesson074 : IThrowingWeaponLesson074
+        {
+            public void Fire()
+            {
+                Console.WriteLine($"{GetType().Name}: Хыщ!");
+            }
+
+            public void Throw()
+            {
+                Console.WriteLine($"{GetType().Name}: Фьють!");
+            }
+        }
+
+        class PlayerLesson074
+        {
+            public void Fire(IWeaponLesson074 weapon)
+            {
+                weapon.Fire();
+            }
+
+            public void Throw(IThrowingWeaponLesson074 throwingWeapong)
+            {
+                throwingWeapong.Throw();
+            }
+        }
+
+        // Интерфейсы. ЯВНАЯ РЕАЛИЗАЦИЯ интерфейса
+        public static void Lesson_075()
+        {
+            Console.WriteLine("Hello from Lesson_075  Интерфейсы. ЯВНАЯ РЕАЛИЗАЦИЯ интерфейса");
+            Console.WriteLine();
+
+            // Явная реализация интерфейса позволяет реализовать методы интерфейса таким образом,
+            // чтобы они были доступны только через ссылку на интерфейс, а не через объект класса.
+
+            MyClassLesson075 myClass = new MyClassLesson075();
+            Foo(myClass);
+            Bar(myClass);
+
+            Console.WriteLine();
+
+            MyOtherClassLesson075 myOtherClass = new MyOtherClassLesson075();
+            Foo(myOtherClass);
+            Bar(myOtherClass);
+
+            Console.WriteLine();
+
+            IFirstInterface firstInterface = myOtherClass;
+            firstInterface.Action(); // Вызывает метод Action() из IFirstInterface
+
+            // или так с помощью приведения типа
+            ((IFirstInterface)myOtherClass).Action(); // Вызывает метод Action() из IFirstInterface
+            Console.WriteLine();
+            ((ISecondInterface)myOtherClass).Action(); // Вызывает метод Action() из ISecondInterface
+
+            // или так безопасней
+           
+            if (myOtherClass is IFirstInterface first2Interface)
+            {
+                first2Interface.Action();
+            }
+
+            if (myOtherClass is ISecondInterface first3Interface)
+            {
+                first3Interface.Action();
+            }
+
+
+            object obj = new object();
+
+            if (obj is IFirstInterface first1Interface)
+            {
+                first1Interface.Action();
+            }
+
+            Console.WriteLine(new string('-', 120));
+        }
+
+        interface IFirstInterface
+        {
+            void Action();
+        }
+
+        interface ISecondInterface
+        {
+            void Action();
+        }
+
+
+        // Пример неявной реализации интерфейса в C#
+        class MyClassLesson075 : IFirstInterface, ISecondInterface
+        {
+            public void Action()
+            {
+                Console.WriteLine("MyClass Action...");
+            }
+        }
+
+        // Пример явной реализации интерфейса в C#
+        // Важно обратить внимание на отсутствие модификатора доступа public у методов интерфейса в классе MyOtherClassLesson075.
+        // Это означает, что методы Action() будут доступны только через ссылку на интерфейс, а не через объект класса.
+        class MyOtherClassLesson075 : IFirstInterface, ISecondInterface
+        {
+            void IFirstInterface.Action()
+            {
+                Console.WriteLine("MyOtherClass Action for IFirstInterface...");
+            }
+
+            void ISecondInterface.Action()
+            {
+                Console.WriteLine("MyOtherClass Action for ISecondInterface...");
+            }
+        }
+
+        static void Foo(IFirstInterface firstInterface)
+        {
+            firstInterface.Action();
+        }
+
+        static void Bar(ISecondInterface secondInterface)
+        {
+            secondInterface.Action();
         }
     }
 }
